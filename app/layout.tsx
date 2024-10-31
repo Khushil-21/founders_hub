daaf7e553f06800cab66c1d2fcf1585de5b0c471
Khushil-21 <khushilshah2105@gmail.com>
2024-10-31 15:22:54 +0530

    added vercel insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const workSans = localFont({
 	src: [
@@ -29,6 +31,8 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<Analytics />
+			<SpeedInsights/>
 			<body className={workSans.variable}>{children}</body>
 		</html>
 	);
