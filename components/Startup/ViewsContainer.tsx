bd46421bd171cdd880c824bea9cd3adae78ae49a
Khushil-21 <khushilshah2105@gmail.com>
2024-11-01 10:36:10 +0530

    used experimental After from NextJS

--- components/Startup/ViewsContainer.tsx
+++ components/Startup/ViewsContainer.tsx
@@ -3,6 +3,7 @@ import PingAnimation from "./PingAnimation";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 import { client } from "@/sanity/lib/client";
 import { writeClient } from "@/sanity/lib/write-client";
+import { unstable_after as after } from "next/server";
 
 export default async function ViewsContainer({ id }: { id: string }) {
 	const { views: totalViews } = await client
@@ -10,18 +11,24 @@ export default async function ViewsContainer({ id }: { id: string }) {
 		.fetch(STARTUP_VIEWS_QUERY, {
 			id,
 		});
-	await writeClient
-		.patch(id)
-		.inc({ views: totalViews + 1 })
-		.commit();
+
+	after(async () => {
+		await writeClient.patch(id).inc({ views: 1 }).commit();
+		// await writeClient
+		// 	.patch(id)
+		// 	.set({ views: totalViews + 1 })
+		// 	.commit();
+	});
+
 	return (
 		<div className="view-container">
 			<div className="absolute -top-2 -right-2">
 				<PingAnimation />
 			</div>
 
-			<p className="view-text">
-				<span className="font-black">Views: {totalViews}</span>
+            <p className="view-text">
+                {/* temporary fix for now so when user lands on this page already one view is incremented. but we should not use after here first increment should done after that get the latest views and then show the total views */}
+				<span className="font-black">Views: {totalViews+1}</span>
 			</p>
 		</div>
 	);
