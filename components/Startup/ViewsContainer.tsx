06b6e7b730126ef7604d7eb3c99d836935fedf9a
Khushil-21 <khushilshah2105@gmail.com>
2024-11-01 10:25:19 +0530

    write total view when posts is viewed

--- components/Startup/ViewsContainer.tsx
+++ components/Startup/ViewsContainer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import PingAnimation from "./PingAnimation";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 import { client } from "@/sanity/lib/client";
+import { writeClient } from "@/sanity/lib/write-client";
 
 export default async function ViewsContainer({ id }: { id: string }) {
 	const { views: totalViews } = await client
@@ -9,7 +10,10 @@ export default async function ViewsContainer({ id }: { id: string }) {
 		.fetch(STARTUP_VIEWS_QUERY, {
 			id,
 		});
-
+	await writeClient
+		.patch(id)
+		.inc({ views: totalViews + 1 })
+		.commit();
 	return (
 		<div className="view-container">
 			<div className="absolute -top-2 -right-2">
