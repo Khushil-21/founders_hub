93a2b83e9afb522a0f238e6c00bc4897296a214c
Khushil-21 <khushilshah2105@gmail.com>
2024-11-01 10:16:12 +0530

    view will be changed in real time

--- components/Startup/ViewsContainer.tsx
+++ components/Startup/ViewsContainer.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import PingAnimation from "./PingAnimation";
+import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { client } from "@/sanity/lib/client";
 
-export default function ViewsContainer({ id }: { id: string }) {
+export default async function ViewsContainer({ id }: { id: string }) {
+	const { views: totalViews } = await client
+		.withConfig({ useCdn: false })
+		.fetch(STARTUP_VIEWS_QUERY, {
+			id,
+		});
+
 	return (
 		<div className="view-container">
 			<div className="absolute -top-2 -right-2">
@@ -9,7 +17,7 @@ export default function ViewsContainer({ id }: { id: string }) {
 			</div>
 
 			<p className="view-text">
-				<span className="font-black">Views: 100</span>
+				<span className="font-black">Views: {totalViews}</span>
 			</p>
 		</div>
 	);
