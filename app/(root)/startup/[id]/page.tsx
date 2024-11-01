74ecdcc152f12f142aadafa9eb537e71c2f11c84
Khushil-21 <khushilshah2105@gmail.com>
2024-11-01 09:53:42 +0530

    startup by id done , metadata and static params

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,4 +1,36 @@
+import { client } from "@/sanity/lib/client";
+import { STARTUP_BY_ID_QUERY, STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { Startup } from "@/sanity/types";
+import { notFound } from "next/navigation";
+
 export const experimental_ppr = true;
+
+export const generateMetadata = async ({
+	params,
+}: {
+	params: Promise<{ id: string }>;
+}) => {
+	const { id } = await params;
+
+	const startup = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+	if (!startup) notFound();
+
+	return {
+		title: startup.title,
+		description: startup.description,
+	};
+};
+
+export const generateStaticParams = async () => {
+	const startups = await client.fetch(STARTUPS_QUERY, {
+		search: null,
+	});
+
+	return startups.map((startup: Startup) => ({
+		id: startup._id,
+	}));
+};
 
 export default async function page({
 	params,
@@ -7,5 +39,9 @@ export default async function page({
 }) {
 	const { id } = await params;
 
-	return <div>{id}</div>;
+	const startup = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+	if (!startup) notFound();
+
+	return <div>{startup.title}</div>;
 }
