d09884dda9cbb04462da6c52eb8789aa40aa5f1b
Khushil-21 <khushilshah2105@gmail.com>
2024-11-06 12:16:51 +0530

    comments

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,13 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, projectId } from "../env";
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+	projectId,
+	dataset,
+	apiVersion,
+	useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+});
+
+// useCdn: false will not store cache and get new data on every request
+// useCdn: true will store cache and get cached data on every request
