9fd272f8c5c9a1f4343fd1dad6f32498a611dc71
Khushil-21 <khushilshah2105@gmail.com>
2024-11-06 12:06:45 +0530

    back to old logic

--- components/Startup/ViewsContainer.tsx
+++ components/Startup/ViewsContainer.tsx
@@ -3,23 +3,25 @@ import PingAnimation from "./PingAnimation";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 import { client } from "@/sanity/lib/client";
 import { writeClient } from "@/sanity/lib/write-client";
-// import { unstable_after as after } from "next/server";
+import { unstable_after as after } from "next/server";
 
 export default async function ViewsContainer({ id }: { id: string }) {
-	await writeClient.patch(id).inc({ views: 1 }).commit();
+	// TODO: May be there is logic error here
+
+	// await writeClient.patch(id).inc({ views: 1 }).commit();
 	const { views: totalViews } = await client
 		.withConfig({ useCdn: false })
 		.fetch(STARTUP_VIEWS_QUERY, {
 			id,
 		});
 
-	// after(async () => {
-	// 	await writeClient.patch(id).inc({ views: 1 }).commit();
-	// await writeClient
-	// 	.patch(id)
-	// 	.set({ views: totalViews + 1 })
-	// 	.commit();
-	// });
+	after(async () => {
+		await writeClient.patch(id).inc({ views: 1 }).commit();
+	await writeClient
+		.patch(id)
+		.set({ views: totalViews + 1 })
+		.commit();
+	});
 
 	return (
 		<div className="view-container">
